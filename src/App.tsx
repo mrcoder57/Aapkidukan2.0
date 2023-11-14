98d6a99d1ab1d8ce90c50b0bee5e99f88c2eeade
mrcoder57 <tiwariji2300@gmail.com>
2023-11-14 10:10:55 +0530

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       
-      <BrowserRouter>
+      <BrowserRouter basename='/'>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Hero/>}/>
