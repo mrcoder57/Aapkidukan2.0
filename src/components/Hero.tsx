d438132c1ed033dc597ad3450d749ef06f42c02e
mrcoder57 <tiwariji2300@gmail.com>
2023-11-19 14:14:46 +0530

    upadted hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,11 +19,11 @@ interface ProductProps {
 
 const Productcard = (props: ProductProps) => {
   return (
-    <div className="card grid grid-cols-2 bg-base-100 shadow-xl max-h-[400px] m-10 w-[300px]  lg:w-[350px] lg:h-[250px] h-[200px] transition ease-in-out duration-300 hover:scale-105">
-      <figure className="object-cover lg:h-[250px] h-[200px] w-full rounded-lg Propss-center  ">
-        <img className="object-cover rounded-lg h-[200px]" src={props.image} alt={props.title} />
+    <div className="card grid md:grid-cols-1 lg:grid-cols-2 sm:grid-cols-1 bg-base-100 shadow-xl max-h-[400px] m-10 w-[180px]  lg:w-[350px] lg:h-[250px] h-[380px] transition ease-in-out duration-300 hover:scale-105">
+      <figure className="object-cover lg:h-[250px] h-[200px] w-[180px] rounded-lg Propss-center  ">
+        <img className="object-contain rounded-lg h-[200px]" src={props.image} alt={props.title} />
       </figure>
-      <div className="card-body w-44 mt-[-25px] ">
+      <div className="card-body w-44 md:w-44 lg:w-44 mt-[-25px] ">
         <h2 className="card-title">{props.title.slice(0, 40)}</h2>
         <h2>{props.description.slice(0,25)}</h2>
         <div className="card-actions ">
@@ -50,25 +50,25 @@ const Hero = () => {
   useEffect(() => {
     getApi();
   }, []);
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 668 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 668, min: 0 },
-      items: 1
-    }
-  };
+  // const responsive = {
+  //   superLargeDesktop: {
+  //     // the naming can be any, depends on you.
+  //     breakpoint: { max: 4000, min: 3000 },
+  //     items: 5
+  //   },
+  //   desktop: {
+  //     breakpoint: { max: 3000, min: 1024 },
+  //     items: 3
+  //   },
+  //   tablet: {
+  //     breakpoint: { max: 1024, min: 668 },
+  //     items: 2
+  //   },
+  //   mobile: {
+  //     breakpoint: { max: 668, min: 0 },
+  //     items: 1
+  //   }
+  // };
   return (
     <div className="h-full w-full">
       <Carouselimg></Carouselimg>
@@ -77,10 +77,10 @@ const Hero = () => {
         <Category/>
       </div>
       
-      <div className="overflow-x-hidden ">
+      <div className="overflow-x-hidden overflow-y-hidden justify-center items-center ">
       <h1 className=" ml-6 mt-4 text-2xl font-bold">Hot Deals  </h1>
-      <div className=" gap-6 w-full mr-5 my-4">
-      <Carousel responsive={responsive}>
+      <div className="grid grid-cols-2 lg:grid-cols-3 md:grid-cols-2 justify-center lg:gap-6 md:gap-4 gap-1 ml-[-20px] w-full md:ml-3 md:mr-6 lg:ml-3 my-4 mr-3">
+      
         {data.map(
           ({ id, title, price, description, category, image, rating }) => (
             <Productcard
@@ -93,7 +93,7 @@ const Hero = () => {
               rating={rating}
             />
           )
-        )}</Carousel>
+        )}
          </div>
       </div>
       
